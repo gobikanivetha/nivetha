a444115ac0cbe4ead9c61973b58aaafacbb66a99
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-01-18 16:35:33 +0530

    Create char.c

---
+++ char.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+char c;
+printf("\n Enter the characters...");
+if(char==0)
+{
+ printf("\n the character is alphabet");
+ }
+ else
+ {
+  printf("\n the character isnot an alphabet");
+ }
+ }
+  
+  
+  
+  
+  
