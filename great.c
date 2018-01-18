8fef4eb6bb138671fc769305730a9933e145d188
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-01-18 15:54:55 +0530

    great.c

---
+++ great.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+ int a,b,c;
+ printf("\nEnter three numbers");
+ scanf("%d%d%d",&a,&b,&c);
+ if(a>b&&a>c)
+ {
+  printf("\n a is largest");
+ }
+ else
+ {
+  printf(" b is larget");
+ }
+ return c;
+ }
