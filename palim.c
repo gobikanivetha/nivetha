b6f58b4ae6c6e7fbf43e1516c357950540b4cfd1
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-01-19 16:42:32 +0530

    Create palim.c

---
+++ palim.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+ int n,r=0,rem;
+ while(n!=0)
+ {
+  rem=!=0;
+  r=r*10+rem;
+  n=n/10;
+  }
+ if(n==r)
+ {
+  printf("\n num is palindrome");
+  }
+  else
+  {
+   printf(" the num is not a palindrome");
+   }
+   return 0;
+   }
+ 
