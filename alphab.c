6678b24e8599b7628a3a7f5fa443881f5f4e6b07
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-01-19 16:07:18 +0530

    Rename alpha.c to alphab.c

---
+++ alphab.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+ char a[][]={"alphabets"};
+ int count;
+ clrscr();
+ printf("\n the given letters are...");
+ if(count==0)
+ {
+  printf(" the given letter is a alphabet");
+	}
+	else
+	{
+	 printf("the given leter is not an alphabet");
+	 }
+	 }
+ 
