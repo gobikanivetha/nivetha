acac0b2dcbc253eba3e2a24b69aa76e22c481e68
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-01-19 15:58:00 +0530

    Create arr.c

---
+++ arr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+ int a[5][8],count;
+ clrscr();
+ printf("\n The no.of words in the array is..");
+ if(count==0)
+ {
+  printf("\n the total no of letters are....");
+	}
+	else
+	{
+	 return 0;
+	 }
+	 }
+ 
