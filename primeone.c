23745917978e7149925a15ce9fb4330e940ac8fb
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-01-21 10:04:43 +0530

    Rename prime.c to primeone.c

---
+++ primeone.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+[
+ int num;
+ printf("\n Enter the number");
+ for(i=0;i<=0;i++)
+ {
+  i=0;
+  c++;
+  }
+  if(i!+0)
+  {
+   printf("the number is prime");
+   }
+   else
+   {
+    return 0;
+    }
+    }
+    
+ 
