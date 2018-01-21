a5f80dd379affe1da5661bc725b7efd010904dbd
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-01-21 11:55:15 +0530

    Create nivig.c

---
+++ nivig.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+[
+ int a[100],max,n,i;
+ printf("\n the numbers are...");
+ scanf("%d",&n);
+ for(i=0;i<=n;i++)
+ {
+  scanf("%d",a[i]);
+  }
+  maxa[i]=0;
+  for(i=0;i<=n;i++)
+  {
+   if(a[i]>max)
+   {
+    max=a[i];
+    {
+     printf("%d",max[i]);}\
+     }
+     }
+     }
