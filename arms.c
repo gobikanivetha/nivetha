6cb160b7a4433977241971e5bebb370468b94617
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-01-21 10:22:31 +0530

    Create arms.c

---
+++ arms.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+ int num;
+ printf("\n Enter three  digit numbers");
+ scanf("%d","%d","%d",&num,&num,&num);
+ original number=n;
+ if(n!=0)
+ {
+  result+="r*r*r";
+  sum=num+sum;
+  }
+  else
+  {
+   return 0;
+   }
+   }
