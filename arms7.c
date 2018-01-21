930e1b54e472d3d69020e4bd6090792c93b2924d
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-01-21 10:31:24 +0530

    Rename arms.c to arms7.c

---
+++ arms7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+ int remainder,
+ printf("\n Enter the number");
+ scanf("%d",num);
+ remainder=total+i;
+ total=remainder;
+ }
+ if(n=armstrong)
+ {
+  printf("\n the number is armstrong");
+  }
+  else
+  {
+   printf("\n the number is not an armstrong");
+   }
+   return 0;
+   }
+   
