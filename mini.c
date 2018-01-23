e433f27bb0588131f72005ac8d9f1e32e69ebf9d
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-01-23 15:59:47 +0530

    Create mini.c

---
+++ mini.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+ int a[i],n, min;
+ printf("\n Enter the number...");
+ scanf("%d"&n);
+ printf("\n Enter the elements");
+ for(i=0;i<=5;i++)
+ {
+  scanf("%d",&a[i]);
+  }
+   min=a[i];
+   for(i=0;i<=n;i++)
+   {
+    if(a[i]>min)
+    { 
+     return min;
+     }
+    }
