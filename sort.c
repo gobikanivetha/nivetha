f29c3b3f230e70644240c45498231fd52a2c3137
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-01-23 16:24:46 +0530

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+ int n,i,j;
+ printf("\n Enter the numbers");
+ scanf("%d",&n);
+ printf("\n Enter the elements");
+ for(i=0;i<=n;i++)
+ {
+  scanf("%d",a[i])
+  }
+  for(i=0;i<=n;i++)
+  {
+   for(j=o;j<n;j++)
+   {
+    scanf("%d",a[j]);
+   }
+   if(a[i]<a[j])
+   {
+    return a[i];
+   }
+   }
