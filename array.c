ee5159928ce67e447b22ea5fb5eafdec4fdd1a8a
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-01-25 15:52:53 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+ int a[max size];
+ printf("\n Enter the numbers..")
+ scanf("%d",&n);
+ for(i=0;i<=n;i+=+)
+ {
+  scanf("d',&a[i]);
+  }
+  printf("\n The elements are...");
+  for(j=0;j<n;j++)
+  {
+   printf("\n The elements with their index value...");
+   }
+   }
