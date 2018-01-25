c61e848f674099d146dab93be96128ee55606d54
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-01-25 16:01:26 +0530

    Create hour.c

---
+++ hour.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+ int h1,m1,s1,h2,m2,s2,h3,m3,s3;
+ printf("\n time 1 is......");
+ scanf("%d%d%d",&h1,&m1,&s1);
+ printf("\n time 2 is.....");
+ scanf("%d%d%d",&h2,&m2,&s2);
+ m3=(m1+m2)/60%;
+ printf("\n time 3 is..., m3");
+ return 0;
+ }
