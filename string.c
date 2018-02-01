07aa4e04062f014e2329d44d7acb3e0a88f48c4a
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-02-01 11:04:07 +0000

    Create string.c

---
+++ string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+ int a;
+ char b{40];
+ printf("\n Enter the string");
+ for(i=0;i<n;i++)
+ { 
+ scanf("%d",&b[i]);
+ }
+ if(b[i]>0)
+ {
+  printf(" the number of characters",%d',b[i]);
+  }
+  }
