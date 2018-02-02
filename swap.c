88e91f8f8aa0ae4c26e06ad6814a901cbc9f5a57
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-02-02 09:33:57 +0000

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+ int a,b,temp;
+ printf("\n Enter the value of a");
+ scanf("%d",&a);
+ print("\n Enter the value of b");
+ scanf("%d",&b);
+ temp=a;
+ a=b;
+ b=temp;
+ printf(" The swapping number is.....%d\%d",a,b);
+ }
