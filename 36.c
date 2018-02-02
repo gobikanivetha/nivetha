dcf225dcf747681224fb64ccb4798c78d5611231
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-02-02 09:18:12 +0000

    Create 36.c

---
+++ 36.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+ char str[100];
+ int i,coount=0;
+ printf("\n Enter the string");
+ scanf("%d",&str);
+ for(i=0;str[i]!='\0';i++)
+ {
+  if(str[i]>='0'&&str[i]<=9)
+  {
+   count++;
+   }
+   printf("%d", count);
+   }
+   
