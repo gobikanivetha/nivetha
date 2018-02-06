adb5d21dcf8eabcb48d3e78ffa19b8ce327cbd3a
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-02-06 09:34:33 +0000

    Create maxf.c

---
+++ maxf.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+ int a[10],i,max;
+ printf("\n Enter the numbers");
+ for(i=0;i<=10;i++)
+ {
+ scanf("%d",&a[i]);
+ }
+ max=a[0]
+ {
+  if(a[i]=max)
+  {
+   max=a[i];
+   }
+   }
+   printf("max=%d",max);
+   return 0;
+   }
+ 
