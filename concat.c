2044cae958503ee06e25551f07ff60c3f8bf639f
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-02-08 15:10:00 +0530

    Create concat.c

---
+++ concat.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+ char str1[40],str2[30],i,j,str3;
+ printf("\n Enter the first string");
+ scanf("%d",&tr1);
+ printf("\n Enter the second string");
+ scanf("%d",&str2);
+ str3=str1+str2;
+ printf("%d",str3);
+ }
+ 
