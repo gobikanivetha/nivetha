f423ed7db7f7b37de5840d7cde7f795fba73f48f
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-02-08 14:31:57 +0530

    Create fib.c

---
+++ fib.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ int main()
+{
+int i,n1,n2,t,n=10,a[10],b[30];
+printf("\n Enter the first  number");
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("\n Enter the second number");
+for(i=0;i<=n;i++)
+{
+scanf("%d",&b[i]);
+}
+t=n1+n2;
+printf("%d",t);
+return 0;
+}
