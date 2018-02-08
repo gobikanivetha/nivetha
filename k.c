55bf4aa50013cffd063a454b540b0f863d9dd460
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-02-08 14:44:01 +0530

    Create k.c

---
+++ k.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int k,i;
+ char s[100];
+ printf("\n Enter the string");
+ scanf("%s",&s);
+ printf("\n Enter the no.f.digits");
+ scanf("%d",&k);
+ for(i=0;i<=k;i++)
+ {
+  printf("%d\n",s);
+  }
+  return 0;
+  }
