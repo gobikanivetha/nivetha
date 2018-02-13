d116e8dedc7d4d74984482173084a7bfb50db5bb
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-02-13 09:34:15 +0000

    Rename power.c2 to power2.c

---
+++ power2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+ int n;
+ printf("\n Enter the number");
+ scanf("%d",&n);
+ if(n%2==0)
+ {
+  printf("\n Power of n "%d",n);
+  } 
+  else
+  {
+   printf("\n Power of n %d",n);
+   }
+   }
