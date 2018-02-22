af1fbd1231ae059b42ad7f448727b8366d59ce21
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-02-22 16:39:06 +0530

    Create fibo.c

---
+++ fibo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+[
+ int i,t1=0,t2=1,series;
+ printf("\n Enter the number of series");
+ scanf("%d",&n);
+ for(i=0;i<=n;i++)
+ {
+   t=t1+t2;
+   t1=t2;
+   t2=series;
+ }
+ return 0;
+ }
+ 
