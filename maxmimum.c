e1a3202073150ae0e266e8373d6dfaf6a52d81f5
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-02-22 16:29:05 +0530

    Create maxmimum.c

---
+++ maxmimum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int mai()
+{
+   int i,max=0,a;
+   printf("\n Enter 10 numbers");
+   for(i=0<i<=10;i++)
+   scanf("%d",&a[i]);
+   for(i=0;i<=10;i++)
+   {
+     max=0;
+     if(a[i]>max)
+     {
+      printf("\n maxmimum number is%d", max); 
+     }
+   }
+   return 0;
+}
