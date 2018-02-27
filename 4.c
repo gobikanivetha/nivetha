7d922cce2704533531aa58d4e8bc69d524902813
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-02-27 16:24:52 +0530

    Create new file

---
+++ 4.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+   int n;
+   printf("\n Enter the number");
+   scanf("%d",&n);
+   printf("\n%d",n);
+   switch(n)
+   {
+     case 1:
+     printf("\n one");
+     break;
+     case 2:
+     printf("\n two");
+     break;
+     case 3:
+     printf("\n three");
+     break;
+     case 4:
+     printf("\n four");
+     break;
+     case 5:
+     printf("\n five");
+     break;
+     case 6:
+     printf("\n six");
+     break;
+     case 7:
+     printf("\n seven");
+     break;
+     case 8:
+     printf("\n eight");
+     break;
+     case 9:
+     printf("\n nine");
+     break;
+   }
+   return 0;
+}
