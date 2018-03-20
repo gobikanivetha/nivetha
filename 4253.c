735b281448a31173bde093d9f982751f9eff9ca4
gobikanivetha <31654453+gobikanivetha@users.noreply.github.com>
2018-03-20 15:52:36 +0530

    Create 4253.c

---
+++ 4253.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int kabali_ninja,oppent_ninja,i,t;
+  for(i=0;i<=3;i++)
+  {
+  scanf("%d%d",&kabali_ninja,&oppent_ninja);
+  printf("%d",oppent_ninja-kabali_ninja);
+  }
+  return 0;
+}
